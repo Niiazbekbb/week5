435a4d028108ca41cca43136e73555784f24fb00
Niiazbekbb <144800349+Niiazbekbb@users.noreply.github.com>
2023-10-10 19:15:01 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,6 @@
-
+user_input = input("Please enter a string: ")
+print(f"Input string = {user_input}")
+index = len(user_input) - 1  
+while index >= 0:
+    print(user_input[index])
+    index -= 1
